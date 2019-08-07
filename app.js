b6765740c9260c9a11fbe6cdc374042a2b3bc178
Juha Wiberg <juha.wiberg@ambientia.fi>
2019-08-07 13:32:07 +0300

    Removed unused variable.

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const port = process.env.PORT || 3000
 const app = express()
 
 app.get('/booking_reference', function (req, res) {
-  let r = Math.random().toString(36).substring(7);
   res.send(makeid(7))
 })
 
@@ -11,12 +10,12 @@ app.listen(port, function () {
   console.log('Example app listening on port ' + port + '!')
 })
 
-function makeid(length) {
-  let result           = '';
-  const characters       = 'abcdefghijklmnopqrstuvwxyz0123456789';
-  const charactersLength = characters.length;
-  for ( var i = 0; i < length; i++ ) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+function makeid (length) {
+  let result = ''
+  const characters = 'abcdefghijklmnopqrstuvwxyz0123456789'
+  const charactersLength = characters.length
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength))
   }
-  return result;
+  return result
 }
