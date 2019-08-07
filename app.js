965c9f864cd256e3a01231d1bf4f148d026cb7a7
Juha Wiberg <juha.wiberg@ambientia.fi>
2019-08-07 13:03:05 +0300

    Booking reference generation algorithm changed.

--- app.js
+++ app.js
@@ -4,8 +4,19 @@ const app = express()
 
 app.get('/booking_reference', function (req, res) {
   let r = Math.random().toString(36).substring(7);
-  res.send(r)
+  res.send(makeid(7))
 })
+
 app.listen(port, function () {
   console.log('Example app listening on port ' + port + '!')
 })
+
+function makeid(length) {
+  let result           = '';
+  const characters       = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
